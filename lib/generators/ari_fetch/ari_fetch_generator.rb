95919127f5e83a344db32908603473ab8d633d7e
keviv vivek <vivekpatna52@gmail.com>
2013-06-03 15:41:50 +0530

    changed migration file name

--- lib/generators/ari_fetch/ari_fetch_generator.rb
+++ lib/generators/ari_fetch/ari_fetch_generator.rb
@@ -17,6 +17,6 @@ class AriFetchGenerator < Rails::Generators::Base
   end
 
   def create_migration_file
-    migration_template 'migration.rb', 'db/migrate/create_ari_files_table.rb'
+    migration_template 'migration.rb', 'db/migrate/create_ari_files.rb'
   end
 end
